8379601e675379399ccfb415e84d9ae0f814ffa8
Ljupcho <ljupco.jakjovski@gmail.com>
2024-01-05 20:09:31 +0100

    second2 commit

--- weather-app/src/app/weather.service.ts
+++ weather-app/src/app/weather.service.ts
@@ -16,7 +16,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class WeatherService {
-  private apiKey = '054ff2fe662d61440e41bd7d1dde3f9d';
+  private apiKey = '';
   private baseUrl = 'https://api.openweathermap.org/data/2.5/';
   constructor(private http: HttpClient) {}
   getCurrentWeather(city: string): Observable<any> {
